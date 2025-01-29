83c547c599216ce86472f19d07d997935f266886
Luiz Vinicius <viniciusgba2008@gmail.com>
2025-01-29 19:47:17 -0300

    tipos primitivos

---
+++ src/estudos/maratonajava/introducao/TiposPrimitivos.java
@@ -0,0 +1,24 @@
+package estudos.maratonajava.introducao;
+
+// aula 02
+
+/*
+     tipos primitivos
+        // - `byte`
+        //- `short`
+        //- `int`
+        //- `long`
+        //- `float`
+        //- `double`
+        //- `char`
+        //- `boolean`
+ */
+
+public class TiposPrimitivos {
+
+    public static void main(String[] args) {
+           int idade = 10;
+           System.out.println("a idade é" + idade+ " anos");
+
+    }
+}
