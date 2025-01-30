b0cfd6eb72f1d19449b4ebf454f5fa3951743e11
Luiz Vinicius <viniciusgba2008@gmail.com>
2025-01-30 18:44:00 -0300

    feat: adicionando alguns exemplos de operadores de atribuição

--- src/estudos/maratonajava/introducao/Operadores.java
+++ src/estudos/maratonajava/introducao/Operadores.java
@@ -43,5 +43,46 @@ public class Operadores {
 
         System.out.println("DentroDaleiMaiorqueTrinta "+isDentroDaLeiMaiorQueTrinta);
         System.out.println("DentroDaleiMenorqueTrinta "+isDentroDaLeiMenorQueTrinta);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlaystation = 5000;
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente >= valorPlaystation || valorTotalContaPoupanca >= valorPlaystation;
+
+        System.out.println("Da pra compra o playstation ? "+isPlaystationCincoCompravel);
+
+        // operadores de atribuição = += -= *= /= %=
+        // Apenas para economizar algumas linhas de codigo
+
+        double bonus = 1800;
+        bonus += 1000;
+        bonus -= 1000;
+        bonus /= 2;
+        bonus *= 2;
+        bonus %= 2;
+        System.out.println(bonus);
+
+        int contador = 0;
+        contador += 1; // contador = contador + !
+        contador++; // só funciona de 1 em 1
+        contador--;
+
+        int contador2 = 0;
+
+        System.out.println(contador2++);
+        System.out.println(contador);
+
+        /*
+        o lado do sinal ++ muda conforme o lado que ele é colocado
+        caso ele seja colocado no começo exemplo ++contador, ele diz que primeira a varialvel faz oque tem que fazer e dps incrementar
+        e caso seja colocado no final, ele já incrementa na hora da execução
+
+        o mesmo vale para o decremento (--)
+         */
+
+
+
+
     }
+
 }
