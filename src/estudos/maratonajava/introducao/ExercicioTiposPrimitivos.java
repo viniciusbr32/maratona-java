10a6a4c10b24adcdd7a471c69b1df057b4a23289
Luiz Vinicius <viniciusgba2008@gmail.com>
2025-01-30 15:13:11 -0300

    feat: Adicionando exercicios de tipos primitivos

---
+++ src/estudos/maratonajava/introducao/ExercicioTiposPrimitivos.java
@@ -0,0 +1,25 @@
+package estudos.maratonajava.introducao;
+
+// Exercicios tipos primitivos
+
+/*
+Crie variáveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
+
+Eu <nome>, morando no endereço <endereço>, confirmo que recebi o salário de <salario>, na data <data>.
+
+
+ */
+
+public class ExercicioTiposPrimitivos {
+
+   public  static void main(String[] args) {
+       String nome = "vinicius";
+       String endereco = "Avenida Bla bla bla, 106, Jardim Primavera";
+       double salario = 2500.25;
+       String data = "30/01/2025";
+       String relatorio = "Eu "+nome+" morando no endereço "+endereco+" confirmo que recebi o salario de "+salario+" na data "+data;
+
+       System.out.println(relatorio);
+   }
+
+}
