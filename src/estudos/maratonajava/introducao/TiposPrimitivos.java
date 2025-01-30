b9c6ee5c2b05601bc927e818caf39a32234aef6a
Luiz Vinicius <viniciusgba2008@gmail.com>
2025-01-30 08:05:35 -0300

    feat: alguns exemplos de tipos primitivos

--- src/estudos/maratonajava/introducao/TiposPrimitivos.java
+++ src/estudos/maratonajava/introducao/TiposPrimitivos.java
@@ -18,7 +18,22 @@ public class TiposPrimitivos {
 
     public static void main(String[] args) {
            int idade = 10;
-           System.out.println("a idade é" + idade+ " anos");
+           long numeroGrande = 100000;
+           double salarioDouble = 2000;
+           float salarioFloat = 2500;
+           byte idadeByte = 12;
+           short idadeShort = 10;
+           boolean verdadeiro = true;
+            boolean falso = false;
+            char caractere = '\u0041';
+            char caractereAsci = 87;
+
+            // no char podemos usar unicode ou asci
+
+
+            System.out.println("a idade é" + idade+ " anos");
+             System.out.println("unicode "+ caractere);
+             System.out.println("Asci "+ caractereAsci);
 
     }
 }
