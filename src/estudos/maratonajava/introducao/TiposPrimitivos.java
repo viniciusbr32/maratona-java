b211a08cb96c9950fee7ac7cb616c7ce3aec48ec
Luiz Vinicius <viniciusgba2008@gmail.com>
2025-01-30 08:36:43 -0300

    feat: Strings e mudança do level do java para 8

--- src/estudos/maratonajava/introducao/TiposPrimitivos.java
+++ src/estudos/maratonajava/introducao/TiposPrimitivos.java
@@ -17,23 +17,25 @@ package estudos.maratonajava.introducao;
 public class TiposPrimitivos {
 
     public static void main(String[] args) {
-           int idade = (int) 100000000000000000L;
-           long numeroGrande = 100000L;
-           double salarioDouble = 2000.0D;
-           float salarioFloat = 2500.0f;
-           byte idadeByte = 12;
-           short idadeShort = 32000;
-           boolean verdadeiro = true;
-            boolean falso = false;
-            char caractere = '\u0041';
-            char caractereAsci = 87;
+        int idade = (int) 100000000000000000L;
+        long numeroGrande = 100000L;
+        double salarioDouble = 2000.0D;
+        float salarioFloat = 2500.0f;
+        byte idadeByte = 12;
+        short idadeShort = 32000;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041';
+        char caractereAsci = 87;
+        String nome = "vinicius";
 
-            // no char podemos usar unicode ou asci
+        // no char podemos usar unicode ou asci
 
 
-            System.out.println("a idade é " + idade+ " anos");
-             System.out.println("unicode "+ caractere);
-             System.out.println("Asci "+ caractereAsci);
+        System.out.println("a idade é " + idade + " anos");
+        System.out.println("unicode " + caractere);
+        System.out.println("Asci " + caractereAsci);
+        System.out.println(nome);
 
 
     }
