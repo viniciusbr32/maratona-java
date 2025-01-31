06f12ec94e1ab5cdaf5dd15997c2f2e7b8e1ec1f
Luiz Vinicius <viniciusgba2008@gmail.com>
2025-01-31 08:09:22 -0300

    feat: Operadores ternarios e exemplos

---
+++ src/estudos/maratonajava/introducao/EstruturasCondicionais03.java
@@ -0,0 +1,17 @@
+package estudos.maratonajava.introducao;
+
+public class EstruturasCondicionais03 {
+    public static void main(String[] args) {
+        // doar se salario for maior que 5000
+
+        double  salario = 3000;
+        String mensagemDoar = "Eu vou doar 500 pro dev dojo";
+        String mensagemNaoDoar = "ainda não tenho condiçoes, mas vou ter";
+        // (condicao) ? verdadeiro : falso
+        String resultado = salario > 5000 ? mensagemDoar : mensagemNaoDoar ;
+
+        System.out.println(resultado);
+
+
+    }
+}
