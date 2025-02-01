a34fbb2a942412954e5a0d692f7f0fb82ee8d123
Luiz Vinicius <viniciusgba2008@gmail.com>
2025-02-01 11:07:01 -0300

    feat: exercicio basico estruturas condicionais Switch

---
+++ src/estudos/maratonajava/introducao/EstruturasCondicionais06.java
@@ -0,0 +1,27 @@
+package estudos.maratonajava.introducao;
+
+public class EstruturasCondicionais06 {
+    public static void main(String[] args) {
+        // utilizando switch e  dados os valores 1 a 7, imprima se dia util ou final de semana
+        // considerando 1 como domingo
+
+        byte dia = 6;
+
+        switch (dia) {
+            case 1:
+            case 7:
+                System.out.println("Final de semana");
+                break;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("Dia util");
+                break;
+            default:
+                System.out.println("Invalida");
+                break;
+        }
+    }
+}
