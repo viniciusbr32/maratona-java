9d6f7b30c5255584a0813ef992082d70f00bc9f7
Luiz Vinicius <viniciusgba2008@gmail.com>
2025-02-02 20:08:05 -0300

    feat: Criação de novos package e introdução a orientação a objetos

---
+++ src/estudos/maratonajava/javacore/Aintroducaoclasses/test/EstudanteTeste01.java
@@ -0,0 +1,17 @@
+package estudos.maratonajava.javacore.Aintroducaoclasses.test;
+
+import estudos.maratonajava.javacore.Aintroducaoclasses.dominio.Estudante;
+
+public class EstudanteTeste01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+
+        estudante.nome = "Luffy";
+        estudante.idade = 20;
+        estudante.sexo = 'M';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+    }
+}
