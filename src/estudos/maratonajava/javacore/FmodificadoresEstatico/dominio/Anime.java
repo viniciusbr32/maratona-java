9e9fe5e1f124f26f4411c0900d08026cdb74cae6
Luiz Vinicius <viniciusgba2008@gmail.com>
2025-02-11 16:50:06 -0300

    feat: aula sobre bloco de inicialização static, e exemplos

---
+++ src/estudos/maratonajava/javacore/FmodificadoresEstatico/dominio/Anime.java
@@ -0,0 +1,42 @@
+package estudos.maratonajava.javacore.FmodificadoresEstatico.dominio;
+
+public class Anime {
+    private String nome;
+    private static int[] episodios;
+
+    // 1- alocado espaço em memoria
+    // 2- cada atributo da classe é criado e inicialização com valores default ou oque for passado;
+    // 3- bloco inicialização é executado
+    // 4- construtor é executado
+
+   static {
+        System.out.println("Dentro do bloco de inicialização");
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i + 1;
+        }
+    }
+
+    static {
+        System.out.println("Bloco de inicialização 2");
+    }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public Anime() {
+        for(int episodio: Anime.episodios) {
+            System.out.print(episodio + " ");
+        }
+        System.out.println();
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
---
+++ src/estudos/maratonajava/javacore/FmodificadoresEstatico/teste/AnimeTeste01.java
@@ -0,0 +1,10 @@
+package estudos.maratonajava.javacore.FmodificadoresEstatico.teste;
+
+import estudos.maratonajava.javacore.FmodificadoresEstatico.dominio.Anime;
+
+public class AnimeTeste01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+        Anime anime2 = new Anime();
+    }
+}
