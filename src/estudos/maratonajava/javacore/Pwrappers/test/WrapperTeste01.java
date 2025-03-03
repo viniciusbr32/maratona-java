889f34993b3efd22651e043ff9ee68141b0a99c3
Luiz Vinicius <viniciusgba2008@gmail.com>
2025-03-03 19:23:49 -0300

    Wrrappers

---
+++ src/estudos/maratonajava/javacore/Pwrappers/test/WrapperTeste01.java
@@ -0,0 +1,31 @@
+package estudos.maratonajava.javacore.Pwrappers.test;
+
+public class WrapperTeste01 {
+    public static void main(String[] args) {
+
+        // TIPOS PRIMITIVOS E WRAPPERS
+
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10F;
+        double doubleP = 10D;
+        char charP = 'W';
+        boolean booleanP = false;
+
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 10D;
+        Character charW = 'W';
+        Boolean booleanW = false;
+
+
+        int i = intW;
+
+
+    }
+}
